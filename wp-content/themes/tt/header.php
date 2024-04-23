1c769100dc68747403bb555b5f847d80c7e4299b
webazex <webazex@gmail.com>
2024-04-23 14:20:10 +0300

    add base html theme template

--- wp-content/themes/tt/header.php
+++ wp-content/themes/tt/header.php
@@ -1,2 +1,57 @@
-<?php
-wp_head();
+<!DOCTYPE HTML>
+<!--
+	Halcyonic by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>Halcyonic by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <?php wp_head(); ?>
+</head>
+<body>
+<div id="page-wrapper">
+
+    <!-- Header -->
+    <section id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+
+                    <!-- Logo -->
+                    <h1><a href="index.html" id="logo">Halcyonic</a></h1>
+
+                    <!-- Nav -->
+                    <nav id="nav">
+                        <a href="index.html">Homepage</a>
+                        <a href="threecolumn.html">Three Column</a>
+                        <a href="twocolumn1.html">Two Column #1</a>
+                        <a href="twocolumn2.html">Two Column #2</a>
+                        <a href="onecolumn.html">One Column</a>
+                    </nav>
+
+                </div>
+            </div>
+        </div>
+        <div id="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6 col-12-medium">
+
+                        <!-- Banner Copy -->
+                        <p>We do something really useful, important, and unique. Learn all about it here ...</p>
+                        <a href="#" class="button-large">Go on, click me!</a>
+
+                    </div>
+                    <div class="col-6 col-12-medium imp-medium">
+
+                        <!-- Banner Image -->
+                        <a href="#" class="bordered-feature-image"><img src="images/banner.jpg" alt="" /></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
