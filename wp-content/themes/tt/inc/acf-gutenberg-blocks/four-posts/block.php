27475182cb84712adbb0f298a6ad88f002ba4345
webazex <webazex@gmail.com>
2024-04-26 13:24:40 +0300

    remove comments in code

--- wp-content/themes/tt/inc/acf-gutenberg-blocks/four-posts/block.php
+++ wp-content/themes/tt/inc/acf-gutenberg-blocks/four-posts/block.php
@@ -11,13 +11,6 @@ add_action('acf/init', function (){
 		'mode' => 'edit',
 		'align' => 'full',
 		'render_template' => getGbSection('four-posts'),
-//		'enqueue_assets' => function(){
-//			wp_enqueue_style( 'slider-features', get_template_directory_uri() .'/inc/acf-gutenberg-blocks/features-section/style.css' );
-//			wp_enqueue_style( 'slick-frontpage', get_template_directory_uri() .'/inc/acf-gutenberg-blocks/front-page-slider/slick/slick.css' );
-//			wp_enqueue_style( 'slick-theme-frontpage', get_template_directory_uri() .'/inc/acf-gutenberg-blocks/front-page-slider/slick/slick-theme.css' );
-//			wp_enqueue_script( 'slick-frontpage', get_template_directory_uri() . '/inc/acf-gutenberg-blocks/front-page-slider/slick/slick.min.js', ['jquery'], '', true );
-//			wp_enqueue_script( 'slider-features', get_template_directory_uri() . '/inc/acf-gutenberg-blocks/features-section/scripts.js', ['jquery', 'slick-frontpage'], '', true );
-//		},
 		'supports' => [
 			'align' => false,
 			'multiple' => true,
